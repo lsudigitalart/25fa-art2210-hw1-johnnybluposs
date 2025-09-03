bae6df0706ba1b2029899188d6e1c7c478e0e6e1
johnnybluposs <johnnyblushoes@gmail.com>
2025-09-03 12:53:25 -0500

    made the thing

--- sketch.js
+++ sketch.js
@@ -1,16 +1,20 @@
 function setup() {
   // Put things you want to run once in here. 
   createCanvas(400, 400);
+    fill(255,0,0)
 }
 
 function draw() {
   // Put things you want to over and over here. 
-  bacground(200);
+  background(200);
   //ellipse(x,y,width,height)
   ellipse(100,200,50,50)
+  ellipse(300,200,50,50)
+  ellipse(250,200,75,75)
   ellipse(150,200,75,75)
   ellipse(200,200,125,125)
-  ellipse(250,200,75,75)
-  ellipse(300,200,50,50)
-  //fill(255,0,0) //(r,g,b)
+  ellipse(200)
+  
+  //fill(255,0,0) //fill(r,g,b)
+
 }
